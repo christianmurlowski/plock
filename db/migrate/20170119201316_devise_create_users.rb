cd1061321188b3600783a313f1ddf387ee9a2aec
Christian Murlowski <christianmurlowski@gmail.com>
2017-01-30 23:13:26 +0100

    delete old user table and merge with devise user table

--- db/migrate/20170119201316_devise_create_users.rb
+++ db/migrate/20170119201316_devise_create_users.rb
@@ -1,9 +1,11 @@
-class AddDeviseToUsers < ActiveRecord::Migration[5.0]
+class DeviseCreateUsers  < ActiveRecord::Migration[5.0]
   def self.up
-    change_table :users do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
+      t.string :name
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -27,11 +27,9 @@ ActiveRecord::Schema.define(version: 20170119201316) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
