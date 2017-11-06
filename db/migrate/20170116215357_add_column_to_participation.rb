a6eaad8b7b5553e83c875a03f18564eb60c2057d
Christian Murlowski <cmurlowski@avarteq.de>
2017-11-06 19:41:22 +0100

    adjsut migration

--- db/migrate/20170116215357_add_column_to_participation.rb
+++ db/migrate/20170116215357_add_column_to_participation.rb
@@ -2,6 +2,6 @@ class AddColumnToParticipation < ActiveRecord::Migration[5.0]
   def change
     add_column :participations, :event_id, :integer
     add_column :participations, :user_id, :integer
-    add_column :participations, :owned, null:false, default:false, :integer
+    add_column :participations, :owned, :null => false, :default => false, :integer
   end
 end
